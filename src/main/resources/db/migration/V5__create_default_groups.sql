3919f1690839b979ed212ec52620f25047f10f67
Abduvaliy <inosukebest1@example.com>
2026-09-10 12:24:55 +0500

    tekshirish 8

---
+++ src/main/resources/db/migration/V5__create_default_groups.sql
@@ -0,0 +1,12 @@
+INSERT INTO groups (
+    name,
+    teacher_id,
+    is_active,
+    created_at,
+    update_at
+)
+VALUES
+    ('Guruh 1', NULL, TRUE, NOW(), NOW()),
+    ('Guruh 2', NULL, TRUE, NOW(), NOW()),
+    ('Guruh 3', NULL, TRUE, NOW(), NOW()),
+    ('Guruh 4', NULL, TRUE, NOW(), NOW());
