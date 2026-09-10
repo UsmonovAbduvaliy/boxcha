d15d7db10527fd3dabb7a171001f372e6e136644
Abduvaliy <inosukebest1@example.com>
2026-09-10 11:49:26 +0500

    tekshirish 5

--- src/main/java/com/example/boxcha/BoxchaApplication.java
+++ src/main/java/com/example/boxcha/BoxchaApplication.java
@@ -3,6 +3,7 @@ package com.example.boxcha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +11,8 @@ public class BoxchaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoxchaApplication.class, args);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println( encoder.encode("boxcha001"));
     }
 
 }
